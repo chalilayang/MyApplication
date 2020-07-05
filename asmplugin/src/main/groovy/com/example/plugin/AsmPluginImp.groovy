0287bf62d390bb7a5f6c40087a728f7d6ba3d243
chalilayang <chalilayang@sohu-inc.com>
2020-07-05 16:10:56 +0800

    Initial commit

---
+++ asmplugin/src/main/groovy/com/example/plugin/AsmPluginImp.groovy
@@ -0,0 +1,16 @@
+package com.example.plugin
+
+import com.android.build.gradle.AppExtension
+import org.gradle.api.Plugin
+import org.gradle.api.Project;
+
+class AsmPluginImp implements Plugin<Project> {
+    @Override
+    void apply(Project project) {
+        System.out.println("==LifeCyclePlugin gradle plugin==")
+        def android = project.extensions.getByType(AppExtension)
+        println '----------- registering AutoTrackTransform  -----------'
+        LifeCycleTransform transform = new LifeCycleTransform()
+        android.registerTransform(transform)
+    }
+}
